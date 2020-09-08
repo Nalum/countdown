b99c880187fbc5ad594621e9bb9a7648d174a990
Luke Mallon (Nalum) <luke.mallon@axonista.com>
2020-09-08 14:28:01 +0100

    Create a working time and writing out to file

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"time"
 
@@ -47,7 +48,7 @@ update it as the timer counts down to 0.`,
 			os.Exit(1)
 		}
 
-		f, err := os.Create(outputFile)
+		f, err := os.Create(path)
 		defer f.Close()
 
 		if err != nil {
@@ -55,9 +56,30 @@ update it as the timer counts down to 0.`,
 			os.Exit(2)
 		}
 
-		for t := time.Tick(time.Second); next := range c {
-		}
+		done := make(chan bool)
+		ticker := time.NewTicker(time.Second)
+		go startTimer(ticker.C, done, time.Duration(timerDuration), f)
+		time.Sleep(timerDuration + time.Second)
+		done <- true
+		ticker.Stop()
 	},
+}
+
+func startTimer(ticker <-chan time.Time, done <-chan bool, duration time.Duration, f *os.File) {
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker:
+			data := fmt.Sprintf("%02.0f:%02.0f", math.Floor(duration.Minutes()), duration.Seconds()-(math.Floor(duration.Minutes())*60))
+			f.WriteAt([]byte(data), 0)
+			duration = duration - time.Second
+
+			if duration.Seconds() < 0 {
+				duration = time.Duration(0)
+			}
+		}
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
