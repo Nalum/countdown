509fef7e53613b8a9d04b2ac3c62da4fa4ce434c
Luke Mallon (Nalum) <luke.mallon@axonista.com>
2020-09-08 09:36:19 +0100

    Initial commit with cobra init

---
+++ README.mk
@@ -0,0 +1,34 @@
+# Countdown Timer
+This project was part of a challenge to create a countdown timer.
+
+## Requirements
+
+### Phase 1
+A cli tool that takes optional arguments for duration, output file and format.
+```bash
+$ go run main.go -h                                                                              
+Use this tool to create a count down timer, it will create a file and
+update it as the timer counts down to 0.
+
+Usage:
+  countdown [flags]
+
+Flags:
+  -d, --duration duration    Duration of the count down (default 5m0s)
+  -f, --format string        The format to output the count down timer in (default "mm:ss")
+  -h, --help                 help for countdown
+  -o, --output-file string   Where the output will be written (default "~/.countdown")
+```
+
+The tool should write every second.
+
+### Phase 2
+The following additional features should form phase 2 of the project.
+* The tool should run timers in the background.
+* Multiple timers should be supported.
+* There should be the ability to stop timers.
+
+## Usage
+```
+countdown -h
+```
---
+++ cmd/root.go
@@ -0,0 +1,111 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile       string
+	outputFile    string
+	timerDuration time.Duration
+	outputFormat  string
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "countdown",
+	Short: "This tool is for use with the OBS text source.",
+	Long: `Use this tool to create a count down timer, it will create a file and
+update it as the timer counts down to 0.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := homedir.Expand(outputFile)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		f, err := os.Create(outputFile)
+		defer f.Close()
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(2)
+		}
+
+		for t := time.Tick(time.Second); next := range c {
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "c", "config file (default is $HOME/.countdown.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().StringVarP(&outputFile, "output-file", "o", "~/.countdown", "Where the output will be written")
+	rootCmd.Flags().DurationVarP(&timerDuration, "duration", "d", time.Duration(5)*time.Minute, "Duration of the count down")
+	rootCmd.Flags().StringVarP(&outputFormat, "format", "f", "mm:ss", "The format to output the count down timer in")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".countdown" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".countdown")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
